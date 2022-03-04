b2ca85cfd026f244dba884d629b4a9584a7a8b03
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2022-03-04 10:08:17 +0400

    Check meta tags

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -17,6 +17,34 @@
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
+<!-- Messenger Chat Plugin Code -->
+<div id="fb-root"></div>
+
+<!-- Your Chat Plugin code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+    chatbox.setAttribute("attribution", "biz_inbox");
+</script>
+
+<!-- Your SDK code -->
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v13.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 @inertia
 </body>
 </html>
