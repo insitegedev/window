878acfe13b727f03c28941c60b7a7de137d80dc8
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2022-03-04 10:10:28 +0400

    Check meta tags

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -1,50 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <title>@lang('client.home_meta_title')</title>
-    <meta name="description"
-          content="@lang('client.home_meta_description')">
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     @if(app()->getLocale()=="ge")
-        <link href="{{ mix('/css/AppGeo.css') }}" rel="stylesheet"/>
+        <link href="{{ mix('/css/AppGeo.css') }}" rel="stylesheet" />
     @elseif(app()->getLocale()=="en")
-        <link href="{{ mix('/css/AppEng.css') }}" rel="stylesheet"/>
+        <link href="{{ mix('/css/AppEng.css') }}" rel="stylesheet" />
     @endif
     @routes
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
-<!-- Messenger Chat Plugin Code -->
-<div id="fb-root"></div>
-
-<!-- Your Chat Plugin code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-
-<script>
-    chatbox.setAttribute("attribution", "biz_inbox");
-</script>
-
-<!-- Your SDK code -->
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml            : true,
-            version          : 'v13.0'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
 @inertia
 </body>
 </html>
