299041a37d6a800c89432ebd88d002c7ebff877c
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2022-03-04 11:06:10 +0400

    check meta tags

--- resources/views/client/base.blade.php
+++ resources/views/client/base.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <title>@lang('client.home_meta_title')</title>
+    <meta name="description"
+          content="@lang('client.home_meta_description')">
     @if(app()->getLocale()=="ge")
         <link href="{{ mix('/css/AppGeo.css') }}" rel="stylesheet" />
     @elseif(app()->getLocale()=="en")
