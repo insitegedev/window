d5500fcd47afb674746179ecd46aae35b4b3f7a3
vakho1 <vakhtangi.batsikadze.1@btu.edu.ge>
2022-03-21 17:47:32 +0400

    Sold changes

--- resources/js/pages/FloorPlan/A4.js
+++ resources/js/pages/FloorPlan/A4.js
@@ -123,7 +123,7 @@ const ChooseFloor = ({ seo, urlPrev, apartments }) => {
                                                 fontWeight: "bold",
                                             }}
                                         >
-                                            {sold ? "sold" : flat.dimension}
+                                            {sold ? __("sold") : flat.dimension}
                                             <sup>{sold ? "" : "2"}</sup>
                                         </button>
                                     }
